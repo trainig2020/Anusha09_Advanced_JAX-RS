afb7e1048abb9c4c9f1899dac94332478a190b79
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-14 05:55:22 +0530

    Add files via upload

---
+++ advanced-jaxrs-GenericType/src/main/java/org/anusha/rest/client/GenericDemo.java
@@ -0,0 +1,23 @@
+package org.anusha.rest.client;
+
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
+import org.anusha.messenger.model.Message;
+
+public class GenericDemo {
+	public static void main(String[] args) {
+		 Client client = ClientBuilder.newClient();
+		  
+	     List <Message> messages=  client.target("http://localhost:8080/advanced-jaxrs-GenericType/webapi/")
+		                           .path("messages")
+		                           .queryParam("year",2020)
+		                           .request(MediaType.APPLICATION_JSON)
+		                            .get(new GenericType<List<Message>>() {});
+	     System.out.println(messages);
+	}
+}
